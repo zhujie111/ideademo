36c978dbc16e99be8093596738ce963e640e444f
zhujie111 <2649479493@qq.com>
2018-10-15 15:27:55 +0800

    初次提交

--- springbootdemo3/src/main/java/com/newer/springbootdemo3/controller/AdminsController.java
+++ springbootdemo3/src/main/java/com/newer/springbootdemo3/controller/AdminsController.java
@@ -19,7 +19,7 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/admins")
-//@CrossOrigin(origins = "http://localhost:8080",maxAge = 3600)
+//@CrossOrigin(origins = "http://localhost:8080",maxAge = 3600)//
 public class AdminsController {
     @Autowired
     private AdminsService adminsService;
